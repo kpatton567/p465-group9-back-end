f359bf9c35961a35984d08c78034f23175768337
akerse <akerse@iu.edu>
2020-09-24 12:01:25 -0400

    Initial file structure setup

---
+++ prevue/src/main/java/com/group9/prevue/PrevueApplication.java
@@ -0,0 +1,13 @@
+package com.group9.prevue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevueApplication.class, args);
+	}
+
+}
---
+++ prevue/src/test/java/com/group9/prevue/PrevueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group9.prevue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
