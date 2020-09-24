543402832b15a2f3e89fad72cc896528c969a1b0
akerse <akerse@iu.edu>
2020-09-24 12:45:09 -0400

    Finished testing spring boot

---
+++ prevue/src/main/java/com/group9/prevue/TestController.java
@@ -0,0 +1,13 @@
+package com.group9.prevue;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+}
