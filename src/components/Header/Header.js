0674e21624c11cf48959ed3b05db3afc0009cd3f
Kyle Patton <kpatton567@gmail.com>
2020-10-01 11:23:09 -0400

    Fixing merge issue

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Header() {
+    return(
+        <nav class="navbar navbar-dark bg-dark mb-2 ">
+
+            <div className="row col-12 d-flex justify-content-center text-white">
+            <span className="h3">PreVue</span>
+            </div>
+        </nav>
+    )
+}
+export default Header;
