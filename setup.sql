ea5562a2aff054ee97ecd650158294bf3c69e866
akerse <akerse@iu.edu>
2020-10-30 04:07:03 -0400

    Begin sql setup file

--- setup.sql
+++ setup.sql
@@ -1,3 +1,5 @@
+insert into users values('123456789', 
+
 insert into movies values(1, 'After Thanos snaps, the remaining Avengers assemble to try to find a way to restore the universe.', 'https://images-na.ssl-images-amazon.com/images/I/81ai6zx6eXL._AC_SL1304_.jpg', 'Avengers: Endgame');
 insert into movies values(2, 'Two young sisters find that all the mirrors in their estranged aunts house are covered or hidden. When one of them happens upon a mirror in the basement, she unknowingly releases a malicious demon.', 'https://media-cache.cinematerial.com/p/500x/jllmn2cv/behind-you-movie-poster.jpg?v=1600320617', 'Behind You');
 insert into movies values(3, 'Terror grips a small mountain town as bodies are discovered after each full moon.', 'https://www.metaflix.com/wp-content/uploads/2020/09/The-Wolf-of-Snow-Hollow-Movie-Poster.jpg', 'The Wolf of Snow Hollow');
@@ -8,8 +10,8 @@ insert into movies values(7, 'In post-WWII America, a woman, rebuilding her life
 insert into movies values(8, 'In 1989, a Canadian journalist investigates the circumstances surrounding the suspicious arrest of a heroin addict imprisoned in a Thai jail.', 'https://i.redd.it/4nlz0va50cf41.png', 'Target Number One');
 insert into movies values(9, 'A reformed hunter living in isolation on a wildlife sanctuary becomes involved in a deadly game of cat and mouse when he and the local Sheriff set out to track a vicious killer who may have kidnapped his daughter years ago.', 'https://m.media-amazon.com/images/M/MV5BYjE2MjIwMmYtM2ZiMy00MzdmLTkyNTYtNmFiNjM5MDJhMGVmXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg', 'The Silencing');
 
-insert into theaters values (1, 'IMax');
-insert into theaters values (2, 'AMC');
+insert into theaters values (1, 100, 'IMax', '123456789');
+insert into theaters values (2, 75, 'AMC', '987654321');
 
 insert into showtimes values (1, 7.99, '2020-04-06T09:00:00.000+00:00', 1, 1, 100);
 insert into showtimes values (2, 8.49, '2020-04-07T09:00:00.000+00:00', 1, 1, 100);
