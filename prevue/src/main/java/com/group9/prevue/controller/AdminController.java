c1911d4e00bd2fc434f5a0cd79fd670f0213f7fd
akerse <akerse@iu.edu>
2020-12-07 19:06:06 -0500

    More admin endpoints

--- prevue/src/main/java/com/group9/prevue/controller/AdminController.java
+++ prevue/src/main/java/com/group9/prevue/controller/AdminController.java
@@ -9,6 +9,7 @@ import java.text.ParseException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -223,5 +224,10 @@ public class AdminController {
 
 		return ResponseEntity.ok(new MessageResponse("Payment successful"));
 
+	}
+	
+	@GetMapping("all_payments")
+	public List<Payment> allPayments(@RequestHeader(name = "Authorization") String token) {
+		return paymentRepository.findAll();
 	}
 }
